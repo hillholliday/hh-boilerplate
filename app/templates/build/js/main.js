d7b51995df4ccbfd8f3c148e874c496290c43d77
Rob Erskine <Rob@RobErskine.com>
2014-08-27 15:20:58 -0400

    adding in query string splitter function

--- app/templates/build/js/main.js
+++ app/templates/build/js/main.js
@@ -13,6 +13,23 @@ $(function(){
 		}, 250);
 	}, false);
 
+
+	/*/////////////////////////
+	// Split up query strings
+	/////////////////////////*/
+	var getQueryVariable = function(variable){
+		var query = window.location.search.substring(1);
+		var vars = query.split("&");
+		for (var i=0;i<vars.length;i++) {
+			var pair = vars[i].split("=");
+			if(pair[0] === variable){
+				return pair[1];
+			}
+		}
+
+		return(false);
+	};
+
 	/*/////////////////////////
 	// open data-target in new tabs
 	/////////////////////////*/
