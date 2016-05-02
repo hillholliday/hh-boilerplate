613794322f3242a55e6d56e11ee65cbdd0f61d0e
Max McClaskie <Max.McClaskie@hhcc.com>
2016-05-02 13:25:21 -0400

    removed validate task and html-validation load

--- app/templates/_gruntfile.js
+++ app/templates/_gruntfile.js
@@ -229,7 +229,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-html-validation');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks('grunt-contrib-copy');
@@ -248,7 +247,6 @@ module.exports = function(grunt) {
 
 
   // Custom tasks
-  grunt.registerTask("validate", ['validation']);
   grunt.registerTask("img", ['imagemin','svgmin']);
 
   //watchs
